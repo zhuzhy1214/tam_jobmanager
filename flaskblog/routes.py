c1516360e09923ec4af1f7539e79dbd1457eb25e
zhenyu zhu <zhyzhu@gmail.com>
2021-12-19 16:47:54 -0800

    user add post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,31 +1,20 @@
+import os
+from PIL import Image
 
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'hohoho',
-        'title': 'this is my first post.',
-        'content': 'leave this blank for now',
-        'date_posted': '12-11-2021'
-    },
-    {
-        'author': 'foo',
-        'title': 'this is my bar',
-        'content': 'hello',
-        'date_posted': '12-12-2021'
-    },
-
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts)
+    posts = Post.query.all()
+    print(len(posts))
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
@@ -76,11 +65,55 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    #create a random file name
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+
+        db.session.commit()
+        flash('You account is updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='account', image_file=image_file)
+    return render_template('account.html', title='account', image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('create_post.html', title='New Post', form=form)
 
 
 from flask import jsonify
