d001a8d0d08062fa752c333e34f88bce5ca6c352
zhenyu zhu <zhyzhu@gmail.com>
2021-12-20 17:12:39 -0800

    add pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,13 +1,11 @@
 import os
-from PIL import Image
-
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
 
 @app.route("/")
 @app.route("/home")
@@ -166,9 +164,6 @@ def delete_post(post_id):
     db.session.commit()
     flash('The post has been deleted!', 'success')
     return redirect(url_for('home'))
-
-
-
 
 from flask import jsonify
 @app.route("/api", methods=['GET'])
